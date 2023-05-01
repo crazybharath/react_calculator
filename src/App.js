e405ec4f6ed48d69314e29b1b358024ef666b7ce
Bharath R <118172941+crazybharath@users.noreply.github.com>
2023-05-01 11:38:09 +0530

    its done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css"
+import Calculator from "./components/Calculator/calculator";
+
+const App=()=>{
+  return(
+    <div>
+      <Calculator/>
+    </div>
+  )
+}
+export default App;
